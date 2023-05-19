a1bb06cc62049ffc53c26429efafaff8200c82db
muneebx786 <129435966+muneebx786@users.noreply.github.com>
2023-05-19 16:39:03 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,221 @@
+//**********************************Chapter 1 (Alerts)**************************************
+
+alert ("First Name")
+alert ("Last Name")
+alert ("E-mail")
+alert ("Phone Number")
+alert ("Password")
+
+console.log(alert)
+
+//**********************************Chapter 2 (Variables for string)*******************************
+
+var myVariable;
+myVariable = "Hello, world!";
+
+var teamName = "OpenAI Team";
+alert(teamName);
+bestMan = "John";
+
+console.log (bestMan)
+
+//**********************************Chapter 3 (Variables for numbers)**********************************
+
+var caseQty;
+caseQty = 144;
+var num = 9;
+var sum = 5 + 3;
+
+console.log(sum)
+
+var myNumber = 5;
+myNumber = +10;
+
+console.log(myNumber)
+
+//*******************************Chapter 4 (Variable names Legal and Illegal)*********************************
+
+var firstName = "John";
+var lastName = "Doe";
+var fullName = firstName + lastName;
+
+console.log (firstName,lastName)
+
+//*******************************Chapter 5 (Math Expression i)**********************************
+
+var largeNum = 1000 * 2000;
+var variable1 = 10;
+var variable2 = 5;
+
+var result = variable1 - variable2;
+var number1 = 7;
+var number2 = 3;
+
+var multiplicationResult = number1 * number2;
+
+alert("The result of the multiplication is: " + multiplicationResult);
+console.log (number1,number2)
+
+//**********************************************Chapter 6 (Math Expression II) **************************
+
+var x = 50;
+var y = x++;
+
+var y = 50;
+var z = --y;
+
+console.log(x,y,z);
+
+//*******************************************Chapter 7 (Math Expression III) *******************************
+
+var calculatedNum = 2 + (2 * 6);
+var calculatedNum = (2 + 2) * 6;
+var calculatedNum = (2 + 2) * (4 + 2);
+var calculatedNum = ((2 + 2) * 4) + 2;
+
+console.log(var calculatedNum)
+
+//*******************************************Chapter 8 ((Concatenating Text Strings) *******************************
+
+var num = "2" + "2";
+messsage = ("Hello," + "Dolly");
+alert("33" + 3);
+
+console.log()
+
+//**********************************Chapter 9 (Prompts)**************************************
+
+alert ("First Name")
+alert ("Last Name")
+alert ("E-mail")
+alert ("Phone Number")
+alert ("Password")
+
+console.log(alert)
+
+//**********************************Chapter 10 ( if statements)*******************************
+
+var myVariable;
+myVariable = "Hello, world!";
+
+var teamName = "OpenAI Team";
+if {alert(teamName);
+}
+bestMan = "John";
+
+console.log (bestMan)
+
+//**********************************Chapter 11 (Comparison Operators)**********************************
+
+var caseQty;
+caseQty = 144;
+var num = 9;
+var sum = 5 + 3;
+
+console.log(sum)
+
+var myNumber = 5;
+myNumber = +10;
+
+console.log(myNumber)
+
+//*******************************Chapter 12 (if...else and else if statements)*********************************
+
+var firstName = "John";
+var lastName = "Doe";
+var fullName = firstName + lastName;
+
+console.log (firstName,lastName)
+
+//*******************************Chapter 13 (Testing sets of conditions)**********************************
+
+var largeNum = 1000 * 2000;
+var variable1 = 10;
+var variable2 = 5;
+
+var result = variable1 - variable2;
+var number1 = 7;
+var number2 = 3;
+
+var multiplicationResult = number1 * number2;
+
+alert("The result of the multiplication is: " + multiplicationResult);
+console.log (number1,number2)
+
+//**********************************************Chapter 14 (If statements nested) **************************
+
+var x = 50;
+var y = x++;
+
+var y = 50;
+var z = --y;
+
+console.log(x,y,z);
+
+//*******************************************Chapter 15 (Array I) *******************************
+
+var calculatedNum = 2 + (2 * 6);
+var calculatedNum = (2 + 2) * 6;
+var calculatedNum = (2 + 2) * (4 + 2);
+var calculatedNum = ((2 + 2) * 4) + 2;
+
+console.log(var calculatedNum)
+
+//*******************************************Chapter 15 (Array II) *******************************
+
+var num = "2" + "2";
+messsage = ("Hello," + "Dolly");
+alert("33" + 3);
+
+console.log()
+
+//*******************************Chapter 15 (Array III)**********************************
+
+var largeNum = 1000 * 2000;
+var variable1 = 10;
+var variable2 = 5;
+
+var result = variable1 - variable2;
+var number1 = 7;
+var number2 = 3;
+
+var multiplicationResult = number1 * number2;
+
+alert("The result of the multiplication is: " + multiplicationResult);
+console.log (number1,number2)
+
+//**********************************************Chapter 6 (Math Expression II) **************************
+
+var x = 50;
+var y = x++;
+
+var y = 50;
+var z = --y;
+
+console.log(x,y,z);
+
+//*******************************************Chapter 17 - 20 (for Loops) *******************************
+if {
+    var num = "2" + "2";
+messsage = ("Hello," + "Dolly");
+alert("33" + 3);
+    if{
+        var calculatedNum = 2 + (2 * 6);
+        var calculatedNum = (2 + 2) * 6;
+        var calculatedNum = (2 + 2) * (4 + 2);
+        var calculatedNum = ((2 + 2) * 4) + 2;
+
+    }
+}
+var calculatedNum = 2 + (2 * 6);
+var calculatedNum = (2 + 2) * 6;
+var calculatedNum = (2 + 2) * (4 + 2);
+var calculatedNum = ((2 + 2) * 4) + 2;
+
+console.log(var calculatedNum)
+
+var num = "2" + "2";
+messsage = ("Hello," + "Dolly");
+alert("33" + 3);
+
+console.log()
